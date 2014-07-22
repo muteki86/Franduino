6a48d106551e744b436c3c19d170b62a7ae9ffe9
Francisco Rodriguez <franr@banshuusoft.com>
2014-07-21 19:27:52 -0600

    lulecitas documentación

--- lucecitas/lucecitas.ino
+++ lucecitas/lucecitas.ino
@@ -1,4 +1,5 @@
 
+
 // LEDS
 int led1 = 11;
 int led2 = 12;
@@ -11,17 +12,17 @@ const int buttonPinStep = 3;
 const int buttonPinReset = 4;
 
 // ESTADO BOTONES
-int buttonStepState = 0; 
+int buttonStepState = 0;
 int buttonResetState = 0; 
 int lastStepButtonState = LOW;
 int lastResetButtonState = LOW;
 long lastDebounceTime = 0;
-long debounceDelay = 50;
+long debounceDelay = 50; // velocidad de debounce
 
 // ESTADO LEDS
 int ledState = 0;
 long previousMillis = 0;
-long delayVel = 2000;
+long delayVel = 2000; // velocidad de prendido de los leds
 
 void setup() {
   pinMode(led1, OUTPUT);     
@@ -36,17 +37,24 @@ void setup() {
 }
 
 void CheckButtons(){
+  //leer el estado de los botones
   int readingStep = digitalRead(buttonPinStep);
   int readingReset = digitalRead(buttonPinReset);
 
+  // ver si el estado ha cambiado
   if (readingStep != lastStepButtonState || readingReset != lastResetButtonState) {
     lastDebounceTime = millis();
   }
 
+  //ver si ya paso el tiempo de bounce
   if ((millis() - lastDebounceTime) > debounceDelay) {
+    // logica para el boton de aumentar la velocidad
     if (readingStep != buttonStepState) {
+      //asignar el nuevo valor
       buttonStepState = readingStep;
-      if (buttonStepState == HIGH) {
+      // verificar que se presiono el boton
+      if (buttonStepState == HIGH) {        
+        // rebajar la velocidad de los leds
         if(delayVel > 500){
           delayVel = delayVel - 500;
         }
@@ -56,25 +64,30 @@ void CheckButtons(){
           }
         }
       }
-
     }
+    // logica para el boton de reset
     if (readingReset != buttonResetState) {
       buttonResetState = readingReset;
       if (buttonResetState == HIGH) {
+        // resetear la velocidad de los leds si se presiono
         delayVel = 2000;
       }
     }
-
   }
+  // guardar el valor leido
   lastStepButtonState = readingStep;
   lastResetButtonState = readingReset;
 }
 
 void loop() {
 
+  // verificar el estado de los botones
   CheckButtons();
+  
+  // Obtener la cantidad de milisegundos pasados desde que se inicio el programa
   unsigned long currentMillis = millis();
 
+  // si ya ha pasado mas tiempo que el establecido como velocidad
   if(currentMillis - previousMillis > delayVel) {
     previousMillis = currentMillis;
 
@@ -110,6 +123,7 @@ void loop() {
     }
   }
 }
+
 
 
 
